fb50e5295f2cb5f7b8e71ef14df0ae40f7a20219
matthsp <pouillematthieu@gmail.com>
2024-12-06 02:42:47 -0500

    perf(day 06): remove the clone of death, unused bool

--- README.md
+++ README.md
@@ -20,16 +20,16 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 <!--- benchmarking table --->
 ## Benchmarks
 
-|           Day            |  Part 1   | Part 2 |
-|:------------------------:|:---------:| :---:  |
-| [Day 1](./src/bin/01.rs) | `74.3µs`  | `104.9µs` |
+| Day | Part 1 | Part 2 |
+| :---: | :---: | :---:  |
+| [Day 1](./src/bin/01.rs) | `74.3µs` | `104.9µs` |
 | [Day 2](./src/bin/02.rs) | `246.2µs` | `425.9µs` |
 | [Day 3](./src/bin/03.rs) | `623.4µs` | `687.6µs` |
-| [Day 4](./src/bin/04.rs) | `431.3µs` | `2.2ms` |
+| [Day 4](./src/bin/04.rs) | `406.7µs` | `2.4ms` |
 | [Day 5](./src/bin/05.rs) | `606.8µs` | `722.4µs` |
-| [Day 6](./src/bin/06.rs) | `67.8ms`  | `502.5s` |
+| [Day 6](./src/bin/06.rs) | `140.8µs` | `146.4ms` |
 
-**Total: 6.12ms**
+**Total: 152.84ms**
 <!--- benchmarking table --->
 
 ## Template
--- src/bin/06.rs
+++ src/bin/06.rs
@@ -3,6 +3,7 @@ use std::collections::HashSet;
 advent_of_code::solution!(6);
 
 
+// Parse and find the start pos:
 fn parse_input (input: &str) -> (Vec<Vec<char>>, (i32, i32)) {
     let mut matrix: Vec<Vec<char>> = Vec::new();
     let mut start: (i32, i32) = (0, 0);
@@ -20,12 +21,13 @@ fn parse_input (input: &str) -> (Vec<Vec<char>>, (i32, i32)) {
     (matrix, start)
 }
 
+// Move in a direction, until the guard leaves the matrix, or hit an obstacle
 fn move_direction(matrix: &mut Vec<Vec<char>>, pos: (i32, i32), dir: (i32, i32)) -> ((i32, i32), i32, bool, Vec<(i32, i32)>) {
     let mut move_count = 0;
     let mut new_pos = pos.clone();
-    let mut inbound = true;
     let rows = matrix.len() as i32;
     let cols = matrix[0].len() as i32;
+    // For part two, register all possible pos for an obstacle
     let mut possible_obstacles: Vec<(i32, i32)> = Vec::new();
 
     loop {
@@ -33,20 +35,19 @@ fn move_direction(matrix: &mut Vec<Vec<char>>, pos: (i32, i32), dir: (i32, i32))
         new_pos = (old_pos.0 + dir.0, old_pos.1 + dir.1);
 
         if !(new_pos.0 >= 0 && new_pos.0 < rows && new_pos.1 >= 0 && new_pos.1 < cols) {
-            inbound = false;
-            return ((new_pos.0, new_pos.1), move_count, inbound, possible_obstacles)
+            return ((new_pos.0, new_pos.1), move_count, false, possible_obstacles)
         } else {
             let value = matrix[new_pos.0 as usize][new_pos.1 as usize];
             if value == '#' {
-                return (old_pos, move_count, inbound, possible_obstacles)
+                return (old_pos, move_count, true, possible_obstacles)
             } else if value != 'X' {
+                // I was too lazy to create a set just for that,
+                // instead I updated the value on the matrix (maybe I'll change to a set)
                 possible_obstacles.push(new_pos);
                 move_count = move_count + 1;
                 matrix[new_pos.0 as usize][new_pos.1 as usize] = 'X';
             }
         }
-        let mut clone = matrix.clone();
-        clone[new_pos.0 as usize][new_pos.1 as usize] = 'X';
     }
 }
 
@@ -54,12 +55,14 @@ pub fn part_one(input: &str) -> Option<u32> {
     let (mut matrix, pos) = parse_input(input);
     let mut count = 0;
 
+    // Directions UP, RIGHT, DOWN, LEFT
     let dirs: Vec<(i32, i32)> = vec![(-1,0), (0,1),(1,0),(0,-1)];
 
     let mut current_dir = 0;
     let mut current_pos = pos.clone();
     loop {
         let result = move_direction(&mut matrix, current_pos, dirs[current_dir]);
+        // Get next direction modulo 4 ^^
         current_dir = (current_dir + 1) % dirs.len();
         current_pos = result.0;
         count = count + result.1;
@@ -69,6 +72,7 @@ pub fn part_one(input: &str) -> Option<u32> {
         }
     }
     // I don't understand why I have to + 1 here don't ask :trollface:
+    // without the + 1 it was ok with the sample, but not the real solution
     Some((count + 1) as u32)
 }
 
@@ -86,7 +90,7 @@ fn find_infinite_loops(matrix: &mut Vec<Vec<char>>, pos: (i32, i32), obs: (i32, 
     let mut operations: HashSet<((i32, i32), (i32, i32))> = HashSet::new();
 
     loop {
-        // Repeat found, infinite loop yay so cool omg
+        // Infinite loop found
         if operations.contains(&(current_pos, dirs[current_dir])) {
             return true;
         }
@@ -144,6 +148,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
     }
 }
