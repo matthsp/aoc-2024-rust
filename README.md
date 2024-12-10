55def93db2a23638bfbe8e59c2e4af819839f76c
matthsp <pouillematthieu@gmail.com>
2024-12-10 02:24:54 -0500

    feat(day 10): add example and solution

--- README.md
+++ README.md
@@ -7,17 +7,18 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 <!--- advent_readme_stars table --->
 ## 🎅 2024 Results 🎅
 
-|                     Day                      | Part 1 | Part 2 |
-|:--------------------------------------------:| :---: | :---:  |
-| [Day 1](https://adventofcode.com/2024/day/1) | ⭐ | ⭐ |
-| [Day 2](https://adventofcode.com/2024/day/2) | ⭐ | ⭐ |
-| [Day 3](https://adventofcode.com/2024/day/3) | ⭐ | ⭐ |
-| [Day 4](https://adventofcode.com/2024/day/4) | ⭐ | ⭐ |
-| [Day 5](https://adventofcode.com/2024/day/5) | ⭐ | ⭐ |
-| [Day 6](https://adventofcode.com/2024/day/6) | ⭐ | ⭐ |
-| [Day 7](https://adventofcode.com/2024/day/7) | ⭐ | ⭐ |
-| [Day 8](https://adventofcode.com/2024/day/8) | ⭐ | ⭐ |
-| [Day 8](https://adventofcode.com/2024/day/9) | ⭐ | ⭐ |
+|                      Day                       | Part 1 | Part 2 |
+|:----------------------------------------------:| :---: | :---:  |
+|  [Day 1](https://adventofcode.com/2024/day/1)  | ⭐ | ⭐ |
+|  [Day 2](https://adventofcode.com/2024/day/2)  | ⭐ | ⭐ |
+|  [Day 3](https://adventofcode.com/2024/day/3)  | ⭐ | ⭐ |
+|  [Day 4](https://adventofcode.com/2024/day/4)  | ⭐ | ⭐ |
+|  [Day 5](https://adventofcode.com/2024/day/5)  | ⭐ | ⭐ |
+|  [Day 6](https://adventofcode.com/2024/day/6)  | ⭐ | ⭐ |
+|  [Day 7](https://adventofcode.com/2024/day/7)  | ⭐ | ⭐ |
+|  [Day 8](https://adventofcode.com/2024/day/8)  | ⭐ | ⭐ |
+|  [Day 9](https://adventofcode.com/2024/day/9)  | ⭐ | ⭐ |
+| [Day 10](https://adventofcode.com/2024/day/10) | ⭐ | ⭐ |
 
 <!--- advent_readme_stars table --->
 
@@ -35,8 +36,9 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 | [Day 7](./src/bin/07.rs) | `1.8ms` | `662.6ms` |
 | [Day 8](./src/bin/08.rs) | `59.9µs` | `154.0µs` |
 | [Day 9](./src/bin/09.rs) | `2.4ms` | `101.0ms` |
+| [Day 10](./src/bin/10.rs) | `2.1ms` | `1.9ms` |
 
-**Total: 906.25ms**
+**Total: 910.25ms**
 <!--- benchmarking table --->
 
 ## Template
---
+++ src/bin/10.rs
@@ -0,0 +1,144 @@
+use std::collections::HashMap;
+
+advent_of_code::solution!(10);
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
+struct Pos {
+    x: usize,
+    y: usize,
+    val: usize,
+}
+
+struct Trail {
+    nodes: HashMap<Pos, Vec<Pos>>,
+    trail_end_count: usize,
+}
+
+impl Trail {
+    fn add_node(&mut self, pos: Pos) {
+        self.nodes.entry(pos).or_insert(Vec::new());
+    }
+
+    fn has_node(&mut self, pos: &Pos) -> bool {
+        self.nodes.get(pos).is_some()
+    }
+
+    fn add_edge(&mut self, pos1: Pos, pos2: Pos) {
+        if !self.has_edge(pos1, pos2) {
+            self.nodes.entry(pos1).or_insert_with(Vec::new);
+            self.nodes.entry(pos2).or_insert_with(Vec::new);
+
+            self.nodes.get_mut(&pos1).unwrap().push(pos2);
+            self.nodes.get_mut(&pos2).unwrap().push(pos1);
+        }
+    }
+
+    fn has_edge(&self, pos1: Pos, pos2: Pos) -> bool {
+        if let Some(neighbors) = self.neighbors(&pos1) {
+            neighbors.contains(&pos2)
+        } else {
+            false
+        }
+    }
+
+    fn neighbors(&self, pos: &Pos) -> Option<&Vec<Pos>> {
+        self.nodes.get(pos)
+    }
+}
+
+fn parse(input: &str) -> (Vec<Vec<usize>>, Vec<Pos>) {
+    let mut trailheads: Vec<Pos> = Vec::new();
+
+    let matrix = input.lines().enumerate()
+        .map(|(x, l)| l.chars().enumerate().map(|(y, c)| {
+            if c == '0' {
+                trailheads.push(Pos { x, y, val: 0 });
+            }
+            c.to_digit(10).unwrap() as usize
+        }).collect())
+        .collect() ;
+
+    (matrix, trailheads)
+}
+
+fn get_new_node(matrix: &Vec<Vec<usize>>, trailhead: Pos, offset: (isize, isize)) -> Option<Pos> {
+    let x = trailhead.x as isize + offset.0;
+    let y = trailhead.y as isize + offset.1;
+    if x >= 0 && x < matrix.len() as isize && y >= 0 && y < matrix.len() as isize {
+        if matrix[x as usize][y as usize] == trailhead.val + 1 {
+            return Some(Pos { x: x as usize, y: y as usize, val: matrix[x as usize][y as usize] });
+        }
+    }
+    None
+}
+
+fn traverse_trails(matrix: &Vec<Vec<usize>>, trail: &mut Trail, trailhead: Pos) {
+    for offset in vec![(1, 0), (0, -1), (0, 1), (-1, 0)] {
+        if let Some(pos) = get_new_node(matrix, trailhead, offset) {
+            if pos.val == 9 && !trail.has_node(&pos) {
+                trail.trail_end_count += 1;
+                trail.add_edge(trailhead, pos);
+                continue;
+            }
+            trail.add_edge(trailhead, pos);
+            traverse_trails(matrix, trail, pos);
+        }
+    }
+}
+
+pub fn part_one(input: &str) -> Option<usize> {
+    let (matrix, trailheads) = parse(input);
+
+    Some(trailheads.iter().map(
+        |trailhead| {
+            let mut trail = Trail { trail_end_count: 0, nodes: HashMap::new() };
+            traverse_trails(&matrix, &mut trail, trailhead.clone());
+            trail.trail_end_count
+        }
+    ).sum())
+}
+
+fn count_paths_dfs(matrix: &Vec<Vec<usize>>, trail: &mut Trail, trailhead: Pos) -> usize {
+    if trailhead.val == 9 { return 1; }
+    let mut path_count = 0;
+
+    for offset in &[(0, -1), (0, 1), (-1, 0), (1, 0)] {
+        if let Some(pos) = get_new_node(matrix, trailhead, *offset) {
+            if pos.val == 9 && !trail.has_node(&pos) {
+                trail.trail_end_count += 1;
+            }
+            trail.add_node(pos);
+            trail.add_edge(trailhead, pos);
+            path_count += count_paths_dfs(matrix, trail, pos);
+        }
+    }
+    path_count
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    let (matrix, trailheads) = parse(input);
+
+    Some(trailheads.iter().map(
+        |trailhead| {
+            let mut trail = Trail { trail_end_count: 0, nodes: HashMap::new() };
+            count_paths_dfs(&matrix, &mut trail, trailhead.clone())
+        }
+    ).sum())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
